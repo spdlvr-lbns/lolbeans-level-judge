db55016bddc76c4ff55dff53f04ce233d0594454
spdlvr-lbns <spdlvr83@gmail.com>
2026-09-16 21:36:10 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,50 +1,62 @@
-// Firebase v12
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+
 import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut
-} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+  getAuth
+} from
+"https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
 import {
   getFirestore
-} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+} from
+"https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+import {
+  getStorage
+} from
+"https://www.gstatic.com/firebasejs/12.2.1/firebase-storage.js";
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8xXDmr0TYuIwgg7bVPe1Xoegi3Dkd6Ag",
-  authDomain: "lolbeans-level-judge.firebaseapp.com",
-  projectId: "lolbeans-level-judge",
-  storageBucket: "lolbeans-level-judge.firebasestorage.app",
-  messagingSenderId: "27831466916",
-  appId: "1:27831466916:web:f92e4c417f42fa089e5f31"
+
+  apiKey: "YOUR_API_KEY",
+
+  authDomain:
+    "YOUR_PROJECT.firebaseapp.com",
+
+  projectId:
+    "YOUR_PROJECT_ID",
+
+  storageBucket:
+    "YOUR_PROJECT.firebasestorage.app",
+
+  messagingSenderId:
+    "YOUR_MESSAGING_SENDER_ID",
+
+  appId:
+    "YOUR_APP_ID"
+
 };
 
-const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const app =
+  initializeApp(firebaseConfig);
 
-// Username → Email変換
-function usernameToEmail(username){
-  return username.toLowerCase() + "@lbns.gg";
-}
 
-export async function register(username,password){
-  return createUserWithEmailAndPassword(
-    auth,
-    usernameToEmail(username),
-    password
-  );
-}
+const auth =
+  getAuth(app);
 
-export async function login(username,password){
-  return signInWithEmailAndPassword(
-    auth,
-    usernameToEmail(username),
-    password
-  );
-}
 
-export async function logout(){
-  return signOut(auth);
-}
+const db =
+  getFirestore(app);
+
+
+const storage =
+  getStorage(app);
+
+
+export {
+  auth,
+  db,
+  storage
+};
